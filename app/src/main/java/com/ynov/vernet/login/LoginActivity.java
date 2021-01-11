155a664c77edc7756acaf863034dd8ca03e18d43
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-01-11 21:27:04 +0100

    Adding Firebase in Login & RegisterActivity

--- app/src/main/java/com/ynov/vernet/login/LoginActivity.java
+++ app/src/main/java/com/ynov/vernet/login/LoginActivity.java
@@ -1,18 +1,28 @@
 package com.ynov.vernet.login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText editTextEmail, editTextPsw;
     TextView textViewRegister;
     ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,36 @@ public class LoginActivity extends AppCompatActivity {
         editTextPsw = findViewById(R.id.editTextPsw);
         textViewRegister = findViewById(R.id.textViewRegister);
         progressBar = findViewById(R.id.progressBar);
+
+
+        findViewById(R.id.btnLogin).setOnClickListener(v -> {
+
+            // Get login field
+            String email = editTextEmail.getText().toString();
+            String password = editTextPsw.getText().toString();
+            progressBar.setVisibility(View.VISIBLE);
+
+            // Adding Firebase
+            mAuth = FirebaseAuth.getInstance();
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+        });
+
 
         // Register
         textViewRegister.setOnClickListener(v -> {
--- app/src/main/java/com/ynov/vernet/login/RegisterActivity.java
+++ app/src/main/java/com/ynov/vernet/login/RegisterActivity.java
@@ -1,18 +1,28 @@
 package com.ynov.vernet.login;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText editTextLastName, editTextFirstName, editTextPhone, editTextEmail, editTextPsw;
     TextView textViewLogin;
     ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,36 @@ public class RegisterActivity extends AppCompatActivity {
         editTextPsw = findViewById(R.id.editTextPsw);
         progressBar = findViewById(R.id.progressBar);
         textViewLogin = findViewById(R.id.textViewLogin);
+
+        String email = editTextEmail.getText().toString();
+        String password = editTextPsw.getText().toString();
+        progressBar.setVisibility(View.VISIBLE);
+
+
+        // Adding Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        // Start MainActivity
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.d(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+
+                        progressBar.setVisibility(View.GONE);
+
+                    }
+                });
+
 
         // Login
         textViewLogin.setOnClickListener(v -> {
